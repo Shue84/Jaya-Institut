d0cdaf1d43191802e5b1a0f675f3a5badea52d08
Shue84 <suyanti.witono@bpkpenaburjakarta.or.id>
2025-05-09 12:14:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -292,6 +292,13 @@ with col2:
 
 data = pd.DataFrame(data_dict)
 
+# Define the mapping dictionary for the status codes
+status_map = {
+    0: 'enrolled',
+    1: 'graduate',
+    2: 'dropout'
+}
+
 with st.expander("View the Raw Data"):
     st.dataframe(data=data, width=800, height=10)
 
@@ -301,4 +308,7 @@ if st.button('Predict'):
     new_data = data_preprocessing(data=data)
     with st.expander("View the Preprocessed Data"):
         st.dataframe(data=new_data, width=800, height=10)
-    st.write("Status: {}".format(prediction(new_data)))
+
+    prediction_value = prediction(new_data)[0] # Assuming your prediction function returns a list or array
+    predicted_status = status_map.get(prediction_value, 'Unknown') # Get the word, default to 'Unknown' if not found
+    st.write(f"Status: {predicted_status}")
